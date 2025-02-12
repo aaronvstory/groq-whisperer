f18e3432169bbea1c40d11b2d38567e0ee07ecda
aaronvstory <aaron.v.story@outlook.com>
2025-02-11 21:53:40 -0800

    Initial commit from your app

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+<diff>
+@@ -25,7 +25,7 @@
+   FormControl,
+   FormLabel,
+ } from '@chakra-ui/react';
+-import { useHotkeys } from '@react-use/use-hotkeys';
++import { useHotkeys } from 'react-use';
+ import dynamic from 'next/dynamic';
+ import { InfoIcon, EditIcon } from '@chakra-ui/icons';
+ 
+</diff>
